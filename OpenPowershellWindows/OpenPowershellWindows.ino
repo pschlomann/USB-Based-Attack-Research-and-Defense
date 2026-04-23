d6fd7551a84f5d011070b016dac63b45d00c521e
Phillip Schlomann <phillip.schlomann@gmail.com>
2026-04-22 22:38:49 -0400

    Add files via upload

---
+++ OpenPowershellWindows/OpenPowershellWindows.ino
@@ -0,0 +1,23 @@
+#include "DigiKeyboard.h"
+
+void setup() {
+  // Wait for the machine to detect the DigiSpark, then send a wake-up signal
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(3000);
+
+  // Allow for full keyboard use, then open the Windows run dialog
+  DigiKeyboard.sendKeyStroke(MOD_CONTROL_RIGHT); // If in VirtualBox, will allow for left CTRL usage
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  
+  // Use the run dialog to open a Powershell window
+  DigiKeyboard.print("powershell");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+}
+
+void loop() {
+  
+}
